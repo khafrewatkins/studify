6e8cea328aa729423612d92f8f8ee43fa3e556ce
khafrewatkins <khafrewatkins@gmail.com>
2021-06-02 17:34:18 -0500

    build with backend

---
+++ server.js
@@ -0,0 +1,17 @@
+// Load Node modules
+var express = require('express');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Port website will run on
+app.listen(2121);
+
+const ejs = require('ejs');
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
